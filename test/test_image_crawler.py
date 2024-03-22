afbade85b7fabc5779c86705a8e51be58bd41f8e
nguyen <nguyenhainguyen2606@gmail.com>
2024-03-22 19:53:08 +0700

    add unit test for translation, disable test for image crawling

--- test/test_image_crawler.py
+++ test/test_image_crawler.py
@@ -1,18 +1,18 @@
-from unittest import TestCase
-
-from src.data_crawling.image_crawler import ImageCrawler
-
-
-class TestImageCrawler(TestCase):
-    def test_fetch_all_and_save(self):
-        print("Test function fetch_all_and_save")
-        crawler = ImageCrawler()
-        url_list = [
-            "https://pokemon.fandom.com/vi/wiki/Th%E1%BB%83_lo%E1%BA%A1i:Pok%C3%A9mon"
-        ]
-        # Should run without error
-        crawler.fetch_all_and_save(url_list=url_list,
-                                   substring_condition=["pokemon"],
-                                   endswith_condition=("vi", "png", "jpg"),
-                                   output_csv_file_path="test/output_for_tests/test_images.csv",
-                                   )
+# from unittest import TestCase
+#
+# from src.data_crawling.image_crawler import ImageCrawler
+#
+#
+# class TestImageCrawler(TestCase):
+#     def test_fetch_all_and_save(self):
+#         print("Test function fetch_all_and_save")
+#         crawler = ImageCrawler()
+#         url_list = [
+#             "https://pokemon.fandom.com/vi/wiki/Th%E1%BB%83_lo%E1%BA%A1i:Pok%C3%A9mon"
+#         ]
+#         # Should run without error
+#         crawler.fetch_all_and_save(url_list=url_list,
+#                                    substring_condition=["pokemon"],
+#                                    endswith_condition=("vi", "png", "jpg"),
+#                                    output_csv_file_path="test/output_for_tests/test_images.csv",
+#                                    )
---
+++ test/test_translator.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+import pandas as pd
+from src.data_processing.text.translator import Translator
+
+
+class TestTranslator(TestCase):
+    def test_translate_in_df(self):
+        print("Test function translate_in_df")
+        translator = Translator(from_language="en", to_language="vi")
+        df = pd.DataFrame({"text": ["hello", "hi"]})
+        df = translator.translate_in_df(df=df, column_to_translate="text", column_to_store="translated_text")
+        df['translated_text'] = df['translated_text'].str.lower()
+        print(df)
+        self.assertTrue("chào" in df['translated_text'].values)
+
