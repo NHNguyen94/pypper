183dd6908e5e48286d0daf59a8d58cbee4bef7bd
nguyen <nguyenhainguyen2606@gmail.com>
2024-03-23 02:19:59 +0700

    add unit test for file system handling

--- src/file_handling/file_system_handling.py
+++ src/file_handling/file_system_handling.py
@@ -9,24 +9,32 @@ class FileSystemHandler():
     def __init__(self):
         pass
 
+    def check_folder_existence(self, folder_path: str) -> bool:
+        """
+        Check if a folder exists.
+        :param folder_path: Path to the folder.
+        :return: True if the folder exists, False otherwise.
+        """
+        return os.path.exists(folder_path)
+
     def create_folder_path(self, folder_path: str) -> None:
         """
         Create a folder path if it does not exist.
         :param folder_path: Path to the folder.
         :return: None
         """
-        if not os.path.exists(folder_path):
+        if not self.check_folder_existence(folder_path=folder_path):
             os.makedirs(folder_path)
         else:
             pass
 
-    def delete_folder(self, folder_path: str) -> None:
+    def delete_empty_folder(self, folder_path: str) -> None:
         """
-        Delete a folder.
+        Delete an empty folder.
         :param folder_path: Path to the folder.
         :return: None
         """
-        if os.path.exists(folder_path):
+        if self.check_folder_existence(folder_path=folder_path):
             os.rmdir(folder_path)
         else:
             pass
@@ -37,7 +45,7 @@ class FileSystemHandler():
         :param folder_path: Path to the folder.
         :return: None
         """
-        if os.path.exists(folder_path):
+        if self.check_folder_existence(folder_path=folder_path):
             shutil.rmtree(folder_path)
         else:
             pass
---
+++ test/test_file_system_handling.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from src.file_handling.file_system_handling import FileSystemHandler
+
+
+class TestFileSystemHandler(TestCase):
+    def test_create_folder_path(self):
+        print("Test function create_folder_path")
+        folder_path = "test/output_for_tests/test_folder"
+        file_system_handler = FileSystemHandler()
+        # Create a folder
+        file_system_handler.create_folder_path(folder_path=folder_path)
+        # Check if the folder is created
+        self.assertTrue(file_system_handler.check_folder_existence(folder_path=folder_path))
+        # Delete the folder
+        file_system_handler.delete_not_empty_folder(folder_path=folder_path)
+        # Check if the folder is deleted
+        self.assertFalse(file_system_handler.check_folder_existence(folder_path=folder_path))
