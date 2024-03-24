4ae91ad50cd2ad71cf27d0073141e2cbf4887fd1
nguyen <nguyenhainguyen2606@gmail.com>
2024-03-24 22:15:10 +0700

    add datetime processing

--- src/data_processing/datetime/datetime_processing.py
+++ src/data_processing/datetime/datetime_processing.py
@@ -4,10 +4,12 @@ class DateTimeProcessor():
     def __init__(self):
         pass
 
-    def get_current_utc_timestamp(self) -> str:
+    @staticmethod
+    def get_current_utc_timestamp() -> str:
         current_timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
         return current_timestamp
 
-    def get_current_local_timestamp(self) -> str:
+    @staticmethod
+    def get_current_local_timestamp() -> str:
         current_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         return current_timestamp
