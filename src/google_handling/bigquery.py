dfa94aa32c3dc3c11ae52ca55421bdfc6918bd8c
nguyen <nguyenhainguyen2606@gmail.com>
2024-03-24 23:17:45 +0700

    refactor

--- src/google_handling/bigquery.py
+++ src/google_handling/bigquery.py
@@ -1,20 +1,21 @@
+import io
 import json
 import traceback
-import io
+from pathlib import Path
+
+import pandas
 from airflow import models
-from six import StringIO,BytesIO
-from google.oauth2.service_account import Credentials
-from include.task_scripts.common_task_handler import *
 from google.api_core import retry
 from google.cloud import bigquery
-from pathlib import Path
-import pandas
-
+from google.oauth2.service_account import Credentials
+from include.task_scripts.common_task_handler import *
+from six import StringIO
 
 root_path = str(Path(__file__).resolve().parents[2])
 
+
 class BigQueryError(Exception):
-    '''Exception raised whenever a BigQuery error happened''' 
+    '''Exception raised whenever a BigQuery error happened'''
 
     def __init__(self, errors):
         super().__init__(self._format(errors))
@@ -26,15 +27,16 @@ class BigQueryError(Exception):
             err.extend(error['errors'])
         return json.dumps(err)
 
+
 class BigqueryHandler:
     def __init__(self):
         self.bq_client = self.innit_bigquery_connection()
 
     def innit_bigquery_connection(self):
         creds = self.get_gdrive_creds()
-        self.bq_client = bigquery.Client(credentials = creds)
+        self.bq_client = bigquery.Client(credentials=creds)
         return self.bq_client
-    
+
     def get_gdrive_creds(self) -> Credentials:
         connection_object = models.Connection.get_connection_from_secrets("google_cloud_default")
         creds_str = connection_object.get_extra()
@@ -64,43 +66,42 @@ class BigqueryHandler:
         else:
             return 'Failed job'
 
-    def _insert_rows_into_bigquery(self,bucket_name, file_name):
+    def _insert_rows_into_bigquery(self, bucket_name, file_name):
         blob = self.bq_client.get_bucket(bucket_name).blob(file_name)
 
         row = json.loads(blob.download_as_string())
         print('row: ', row)
         table_id = self.bq_client.dataset('').table('')
         errors = self.bq_client.insert_rows_json(table_id,
-                                    json_rows=[row],
-                                    row_ids=[file_name],
-                                    retry=retry.Retry(deadline=30))
-        
+                                                 json_rows=[row],
+                                                 row_ids=[file_name],
+                                                 retry=retry.Retry(deadline=30))
+
         print(errors)
         if errors != []:
             raise BigQueryError(errors)
-     
 
-    def _load_table_from_uri(self, uri_path, file_name, dataset, tableSchema, tableName, source_format, load_type, max_bad_records_ = 0):
+    def _load_table_from_uri(self, uri_path, file_name, dataset, tableSchema, tableName, source_format, load_type,
+                             max_bad_records_=0):
         # ! source file must be like this:
         """
         {"id": "1", "first_name": "John", "last_name": "Doe", "dob": "1968-01-22", "addresses": [{"status": "current", "address": "123 First Avenue", "city": "Seattle", "state": "WA", "zip": "11111", "numberOfYears": "1"}, {"status": "previous", "address": "456 Main Street", "city": "Portland", "state": "OR", "zip": "22222", "numberOfYears": "5"}]}
         {"id": "2", "first_name": "John", "last_name": "Doe", "dob": "1968-01-22", "addresses": [{"status": "current", "address": "123 First Avenue", "city": "Seattle", "state": "WA", "zip": "11111", "numberOfYears": "1"}, {"status": "previous", "address": "456 Main Street", "city": "Portland", "state": "OR", "zip": "22222", "numberOfYears": "5"}]}
         """
         # ! source file must be the same.
-        #! if source file is not a NEWLINE_DELIMITED_JSON then you need to load it with blob, convert to JSON and then load as file.
+        # ! if source file is not a NEWLINE_DELIMITED_JSON then you need to load it with blob, convert to JSON and then load as file.
         client = self.bq_client
         job_config = bigquery.LoadJobConfig()
-        
 
         uri = f'gs://{uri_path}/{file_name}'
         table_id = client.dataset(dataset).table(tableName)
 
         if source_format == 'CSV':
-            job_config.field_delimiter='\t'
-            job_config.quote_character=None
+            job_config.field_delimiter = '\t'
+            job_config.quote_character = None
 
         if tableSchema is not None:
-            schema = create_schema_from_yaml(tableSchema) 
+            schema = create_schema_from_yaml(tableSchema)
             print(schema)
             job_config.schema = schema
         else:
@@ -114,11 +115,10 @@ class BigqueryHandler:
             uri,
             table_id,
             job_config=job_config,
-            ) 
-            
+        )
+
         load_job.result()
         print(f"Ingestion to BQ table: {table_id} finished.")
-
 
     def _load_table_from_object_string(self, bucket_name, file_name, dataset, tableSchema, tableName):
         client = self.bq_client
@@ -139,7 +139,7 @@ class BigqueryHandler:
         job_config = bigquery.LoadJobConfig()
         table_id = client.dataset(dataset).table(tableName)
 
-        schema = create_schema_from_yaml(tableSchema) 
+        schema = create_schema_from_yaml(tableSchema)
         job_config.schema = schema
 
         job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
@@ -149,7 +149,7 @@ class BigqueryHandler:
             data_file,
             table_id,
             job_config=job_config,
-            ) 
+        )
 
         load_job.result()  # Waits for table load to complete.
         print("Job finished.")
@@ -157,7 +157,7 @@ class BigqueryHandler:
         def _load_table_from_json(self, bucket_name, file_name, dataset, tableSchema, tableName):
             client = self.bq_client
             blob = client.get_bucket(bucket_name).blob(file_name)
-            #! source data file format must be outer array JSON:
+            # ! source data file format must be outer array JSON:
 
             body = json.loads(blob.download_as_string())
             table_id = client.dataset(dataset).table(tableName)
@@ -166,14 +166,14 @@ class BigqueryHandler:
             job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
             job_config.write_disposition = 'WRITE_APPEND'
 
-            schema = create_schema_from_yaml(tableSchema) 
+            schema = create_schema_from_yaml(tableSchema)
             job_config.schema = schema
 
             load_job = client.load_table_from_json(
                 body,
                 table_id,
                 job_config=job_config,
-                )   
+            )
 
             load_job.result()  # Waits for table load to complete.
             print("Job finished.")
@@ -192,10 +192,10 @@ class BigqueryHandler:
         job_config.schema = schema
 
         df = pandas.DataFrame(
-        body,
-        # In the loaded table, the column order reflects the order of the
-        # columns in the DataFrame.
-        columns=["id", "first_name","last_name","dob","addresses"],
+            body,
+            # In the loaded table, the column order reflects the order of the
+            # columns in the DataFrame.
+            columns=["id", "first_name", "last_name", "dob", "addresses"],
 
         )
         df['addresses'] = df.addresses.astype(str)
@@ -213,10 +213,10 @@ class BigqueryHandler:
             df,
             table_id,
             job_config=job_config,
-            ) 
+        )
 
         load_job.result()
-        print("Job finished.")                               
+        print("Job finished.")
 
     def _load_table_as_src(self, bucket_name, file_name, dataset, tableSchema, tableName):
         client = self.bq_client
@@ -226,12 +226,12 @@ class BigqueryHandler:
         body = json.loads(blob.download_as_string())
         table_id = client.dataset(dataset).table(tableName)
         job_config = bigquery.LoadJobConfig()
-        schema = create_schema_from_yaml(tableSchema) 
+        schema = create_schema_from_yaml(tableSchema)
         job_config.schema = schema
 
         job_config.source_format = bigquery.SourceFormat.CSV,
         # something that doesn't exist in your data file:
-        job_config.field_delimiter =";"   
+        job_config.field_delimiter = ";"
         # Notice that ';' worked because the snippet data does not contain ';'
 
         job_config.write_disposition = 'WRITE_APPEND',
@@ -244,7 +244,7 @@ class BigqueryHandler:
             data_file,
             table_id,
             job_config=job_config,
-            ) 
+        )
 
         load_job.result()
         print("Job finished.")
@@ -258,14 +258,14 @@ class BigqueryHandler:
         body = json.loads(blob.download_as_string())
         table_id = client.dataset(dataset).table(tableName)
         job_config = bigquery.LoadJobConfig()
-        schema = create_schema_from_yaml(tableSchema) 
+        schema = create_schema_from_yaml(tableSchema)
         job_config.schema = schema
 
-        df = pandas.io.json.json_normalize(data=body, record_path='addresses', 
-                                meta=[ 'id'	,'first_name',	'last_name',	'dob']
-                                , record_prefix='addresses_'
-                                ,errors='ignore')
-        
+        df = pandas.io.json.json_normalize(data=body, record_path='addresses',
+                                           meta=['id', 'first_name', 'last_name', 'dob']
+                                           , record_prefix='addresses_'
+                                           , errors='ignore')
+
         df = df[
             [
                 'id',
@@ -278,14 +278,14 @@ class BigqueryHandler:
                 'addresses_state',
                 'addresses_zip',
                 'addresses_numberOfYears'
-                ]
+            ]
         ]
 
         load_job = client.load_table_from_dataframe(
             df,
             table_id,
             job_config=job_config,
-            ) 
+        )
 
         load_job.result()
         print("Job finished.")
