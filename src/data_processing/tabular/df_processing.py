63578d445a12b2005f049425eb83099d3081055e
nguyen <nguyenhainguyen2606@gmail.com>
2024-03-25 10:09:13 +0700

    add df processing and unit test

---
+++ src/data_processing/tabular/df_processing.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+class DFProcessor():
+    """
+    A class to process dataframes
+    """
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def drop_outliers(df: pd.DataFrame, column: str, threshold: float) -> pd.DataFrame:
+        """
+        Drop outliers in a dataframe.
+        :param df: The dataframe.
+        :param column: The column to drop outliers.
+        :param threshold: The threshold to consider as an outlier.
+        :return: pd.DataFrame: The dataframe without outliers.
+        """
+        return df[(df[column] < threshold)]
---
+++ test/test_df_processing.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+import pandas as pd
+
+from src.data_processing.tabular.df_processing import DFProcessor
+
+
+class TestDFProcessor(TestCase):
+    def test_drop_outliers(self):
+        print("Test function drop_outliers")
+        df = pd.DataFrame({"A": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]})
+        threshold = 5
+        df_processor = DFProcessor()
+        df_no_outliers = df_processor.drop_outliers(df, "A", threshold)
+        print(df_no_outliers)
+        self.assertTrue(all(df_no_outliers["A"] < threshold))
